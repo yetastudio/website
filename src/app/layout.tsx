7edc311954c1e9fcbfce6f3a6df582d55d8dc8e2
Tobías Facello <tobiasfacellodeveloper@gmail.com>
2025-04-24 23:59:24 -0300

    Adds src folder (inner app folder and files)

---
+++ src/app/layout.tsx
@@ -0,0 +1,62 @@
+
+//! Next Core
+import type { Metadata } from 'next';
+
+//! Metadata
+export const metadata: Metadata = {
+  title: 'Yeta Studio',
+  description: "'Cause 𝗴͟𝗿͟𝗲͟𝗮͟𝘁 brands aren't built on 𝗹𝘂̶𝗰̶𝗸̶.",
+  // metadataBase: new URL('https://'),
+  // verification: {
+  //   google: '',
+  // },
+  openGraph: {
+    title: 'Yeta Studio',
+    description: "'Cause 𝗴͟𝗿͟𝗲͟𝗮͟𝘁 brands aren't built on 𝗹𝘂̶𝗰̶𝗸̶.",
+    // url: 'https://',
+    siteName: 'Yeta Studio',
+    // images: [
+    //   {
+    //     url: new URL(
+    //       ''
+    //     ),
+    //   },
+    // ],
+  },
+  twitter: {
+    title: 'Yeta Studio',
+    description: "'Cause 𝗴͟𝗿͟𝗲͟𝗮͟𝘁 brands aren't built on 𝗹𝘂̶𝗰̶𝗸̶.",
+    // images: [
+    //   new URL(
+    //     ''
+    //   ),
+    // ],
+  },
+};
+
+//! Fonts
+import { Funnel_Display } from "next/font/google";
+
+const funnelDisplay = Funnel_Display({
+  variable: "--font-funnel-display",
+  weight: ["400", "500", "600", "700"],
+  style: ["normal"],
+  subsets: ["latin"],
+});
+
+//! Styles
+import "./globals.css";
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${funnelDisplay.variable}`}>
+        {children}
+      </body>
+    </html>
+  );
+}
