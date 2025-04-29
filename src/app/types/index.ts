137b9656c032758f43971fd5ede9bdd199d26383
Tobías Facello <tobiasfacellodeveloper@gmail.com>
2025-04-29 00:00:17 -0300

    Updates types declarations

---
+++ src/app/types/index.ts
@@ -0,0 +1,18 @@
+//! React Core
+import { ReactNode } from 'react';
+
+//! Lucide React
+import { LucideIcon } from 'lucide-react';
+
+export type ButtonProps = {
+	title?: string;
+	size?: 'sm' | 'md' | 'lg';
+	variant?: 'solid' | 'outline' | 'ghost' | 'link';
+	radius?: 'squared' | 'rounded';
+	isFullWidth?: boolean;
+	isDisabled?: boolean;
+	isLoading?: boolean;
+	icon?: LucideIcon;
+	iconPosition?: 'left' | 'middle' | 'right';
+	onClick?: () => void;
+};
