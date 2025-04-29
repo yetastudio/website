8f2d07e566c16bfa3c416a9c3da6e16819909e57
Tobías Facello <tobiasfacellodeveloper@gmail.com>
2025-04-28 23:58:19 -0300

    Updates (Layout's) Hero component UI (adds subtitle, slogan and CTAs)

--- src/app/components/layout/hero/index.tsx
+++ src/app/components/layout/hero/index.tsx
@@ -1,27 +1,44 @@
-//! Next Components
+//! Components
 import Image from "next/image";
+import { Button } from "@/app/components/ui";
 
 //! Styles
 import styles from "./styles.module.css";
 
 function Hero() {
   return (
-    <div className={styles.titleContainer}>
-      <h1 className={styles.title}>
-        Yeta
-      </h1>
-      <Image
-        className={styles.logo}
-        priority
-        width={50}
-        height={50}
-        quality={100}
-        alt={"Yeta Studio Logo Mark"}
-        src={"https://res.cloudinary.com/doowhwdzj/image/upload/v1745707536/Variant_Dark_wcqmcg.svg"}
-      />
-      <h1 className={styles.title}>
-        Studio
-      </h1>
+    <div className={styles.hero}>
+      <div
+        className={styles.heroContent}>
+        <h2 className={styles.subtitle}>
+          We are
+        </h2>
+        <div className={styles.titleContainer}>
+          <h1 className={styles.title}>
+            Yeta
+          </h1>
+          <Image
+            className={styles.logo}
+            priority
+            width={50}
+            height={50}
+            quality={100}
+            alt={"Yeta Studio Logo Mark"}
+            src={"https://res.cloudinary.com/doowhwdzj/image/upload/v1745707536/Variant_Dark_wcqmcg.svg"}
+          />
+          <h1 className={styles.title}>
+            Studio
+          </h1>
+        </div>
+        <h2 className={styles.slogan}>
+          ‘Cause <u><b>great</b></u> brands
+          aren’t built on <del>luck</del>.
+        </h2>
+        <div className={styles.heroCTAs}>
+          <Button title="Why choose Yeta Studio" radius="rounded" isFullWidth />
+          <Button title="Let's build something" radius="rounded" isFullWidth />
+        </div>
+      </div>
     </div>
   )
 }
