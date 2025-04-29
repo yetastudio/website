0fbacdb362d7ca67888e8a0034aec03cabdf432f
Tobías Facello <tobiasfacellodeveloper@gmail.com>
2025-04-28 23:57:23 -0300

    Adds (Layout's) Header component

--- src/app/components/layout/header/index.tsx
+++ src/app/components/layout/header/index.tsx
@@ -1,8 +1,28 @@
+//! Next Core
+import Image from "next/image";
 
-function Hero() {
+//! Components
+import { Menu } from "lucide-react";
+import { Button } from "@/app/components/ui";
+
+//! Styles
+import styles from "./styles.module.css";
+
+function Header() {
   return (
-    <div>Hero</div>
+    <div className={styles.header}>
+      <Image
+        className={styles.logo}
+        priority
+        width={40}
+        height={40}
+        quality={100}
+        alt={"Yeta Studio Logo Mark"}
+        src={"https://res.cloudinary.com/doowhwdzj/image/upload/v1745585092/Variant_Dark_Background_Transparent_mz2hyt.svg"}
+      />
+      <Button radius={"squared"} variant={"ghost"} icon={Menu} iconPosition="middle" />
+    </div>
   )
 }
 
-export default Hero
+export default Header
