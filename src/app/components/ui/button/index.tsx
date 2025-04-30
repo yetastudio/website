f0fb86628ddea2ff77a620017029a547b387b518
Tobías Facello <tobiasfacellodeveloper@gmail.com>
2025-04-30 01:06:53 -0300

    Updates (Components') UI Button component's UI

--- src/app/components/ui/button/index.tsx
+++ src/app/components/ui/button/index.tsx
@@ -1,23 +1,44 @@
 //! Types
-import { ButtonProps } from "@/app/types"
+import { ButtonProps } from '@/app/types';
 
 //! Styles
-import styles from "./styles.module.css"
+import styles from './styles.module.css';
 
 function Button(props: ButtonProps) {
-  const { title, variant = "solid", size = "md", radius = "squared", icon, iconPosition, isFullWidth, isDisabled, isLoading, onClick } = props
-  const Icon = icon;
-  const iconMiddle = iconPosition === 'middle'
-  const iconLeft = iconPosition === 'left'
-  const iconRight = iconPosition === 'right'
+	const {
+		title,
+		variant = 'solid',
+		size = 'md',
+		radius = 'squared',
+		icon,
+		iconPosition,
+		isFullWidth,
+		isDisabled,
+		isLoading,
+		onClick,
+	} = props;
 
-  return (
-    <button className={`${styles.button} ${styles[size]} ${styles[radius]} ${styles[variant]} ${styles[`icon-${iconPosition}`]} ${isFullWidth ? styles.isFull : null} ${isLoading ? styles.isLoading : null} ${isDisabled ? styles.isDisabled : null}`} onClick={onClick}>
-      {Icon && iconLeft && <Icon />}
-      {Icon && iconMiddle ? <Icon /> : title}
-      {Icon && iconRight && <Icon />}
-    </button>
-  )
+	const Icon = icon;
+	const iconMiddle = iconPosition === 'middle';
+	const iconLeft = iconPosition === 'left';
+	const iconRight = iconPosition === 'right';
+
+	return (
+		<button
+			className={`
+				${styles.button} ${styles[size]} ${styles[radius]} 
+			${styles[variant]} ${styles[`icon-${iconPosition}`]} 
+			${isFullWidth ? styles.isFull : null} 
+			${isLoading ? styles.isLoading : null} 
+			${isDisabled ? styles.isDisabled : null}
+			`}
+			onClick={onClick}
+		>
+			{Icon && iconLeft && <Icon />}
+			{Icon && iconMiddle ? <Icon /> : title}
+			{Icon && iconRight && <Icon />}
+		</button>
+	);
 }
 
 export default Button;
