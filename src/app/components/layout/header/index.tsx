a12ea6510fc3522e589343d14d63b0da7dbb06d4
Tobías Facello <tobiasfacellodeveloper@gmail.com>
2025-04-30 01:09:05 -0300

    Updates (Layout's) Header component's UI (adds mobile and desktop breakpoints)

--- src/app/components/layout/header/index.tsx
+++ src/app/components/layout/header/index.tsx
@@ -1,9 +1,8 @@
-//! Next Core
-import Image from "next/image";
-
 //! Components
-import { Menu } from "lucide-react";
+import Image from "next/image";
+import { NavBar } from "@/app/components/layout";
 import { Button } from "@/app/components/ui";
+import { ArrowUpRight, Menu } from "lucide-react";
 
 //! Styles
 import styles from "./styles.module.css";
@@ -11,16 +10,31 @@ import styles from "./styles.module.css";
 function Header() {
   return (
     <div className={styles.header}>
-      <Image
-        className={styles.logo}
-        priority
-        width={40}
-        height={40}
-        quality={100}
-        alt={"Yeta Studio Logo Mark"}
-        src={"https://res.cloudinary.com/doowhwdzj/image/upload/v1745585092/Variant_Dark_Background_Transparent_mz2hyt.svg"}
-      />
-      <Button radius={"squared"} variant={"ghost"} icon={Menu} iconPosition="middle" />
+      <div className={styles.mobile}>
+        <Image
+          className={styles.logo}
+          priority
+          width={40}
+          height={40}
+          quality={100}
+          alt={"Yeta Studio Logo Mark"}
+          src={"https://res.cloudinary.com/doowhwdzj/image/upload/v1745585092/Variant_Dark_Background_Transparent_mz2hyt.svg"}
+        />
+        <Button radius={"squared"} variant={"ghost"} icon={Menu} iconPosition="middle" />
+      </div>
+      <div className={styles.desktop}>
+        <Image
+          className={styles.logo}
+          priority
+          width={60}
+          height={60}
+          quality={100}
+          alt={"Yeta Studio Logo Mark"}
+          src={"https://res.cloudinary.com/doowhwdzj/image/upload/v1745585092/Variant_Dark_Background_Transparent_mz2hyt.svg"}
+        />
+        <NavBar />
+        <Button title="Get in touch" size="lg" radius={"rounded"} variant={"solid"} icon={ArrowUpRight} iconPosition="right" />
+      </div>
     </div>
   )
 }
