190673835303bc481f0cd8eed20ba1a9b27a646c
Tobías Facello <tobiasfacellodeveloper@gmail.com>
2025-05-14 23:37:30 -0300

    Updates (Layout's) index.ts file

--- src/app/components/layout/index.ts
+++ src/app/components/layout/index.ts
@@ -1,6 +1,7 @@
 import Header from './header';
 import NavBar from './navbar';
 import Hero from './hero';
+import Summary from './summary';
 import Footer from './footer';
 
-export { Header, NavBar, Hero, Footer };
+export { Header, NavBar, Hero, Summary, Footer };
