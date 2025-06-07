4efd3e1087b8067217e4d493a1689cdf5add8fe0
Tobías Facello <tobiasfacellodeveloper@gmail.com>
2025-06-06 23:56:17 -0300

    Updates root Layout metadata (changes metadata thumbnail prop)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,102 +7,109 @@ import { ThemeProvider } from '@/context/ThemeContext';
 
 //! Metadata
 export const metadata: Metadata = {
-  title: '✹ Yeta Studio™',
-  description: "'Cause 𝗴͟𝗿͟𝗲͟𝗮͟𝘁 brands aren't built on 𝗹𝘂̶𝗰̶𝗸̶.",
-  // metadataBase: new URL('https://'),
-  // verification: {
-  //   google: '',
-  // },
-  openGraph: {
-    title: '✹ Yeta Studio™',
-    description: "'Cause 𝗴͟𝗿͟𝗲͟𝗮͟𝘁 brands aren't built on 𝗹𝘂̶𝗰̶𝗸̶.",
-    // url: 'https://',
-    siteName: '✹ Yeta Studio™',
-    images: [
-      {
-        url: new URL(
-          'https://res.cloudinary.com/doowhwdzj/image/upload/v1745707410/thumbnail_v5s8lz.png'
-        ),
-      },
-    ],
-  },
-  twitter: {
-    title: '✹ Yeta Studio™',
-    description: "'Cause 𝗴͟𝗿͟𝗲͟𝗮͟𝘁 brands aren't built on 𝗹𝘂̶𝗰̶𝗸̶.",
-    images: [
-      new URL(
-        'https://res.cloudinary.com/doowhwdzj/image/upload/v1745707410/thumbnail_v5s8lz.png'
-      ),
-    ],
-  },
+	title: '✹ Yeta Studio™',
+	description: "'Cause 𝗴͟𝗿͟𝗲͟𝗮͟𝘁 brands aren't built on 𝗹𝘂̶𝗰̶𝗸̶.",
+	// metadataBase: new URL('https://'),
+	// verification: {
+	//   google: '',
+	// },
+	openGraph: {
+		title: '✹ Yeta Studio™',
+		description: "'Cause 𝗴͟𝗿͟𝗲͟𝗮͟𝘁 brands aren't built on 𝗹𝘂̶𝗰̶𝗸̶.",
+		// url: 'https://',
+		siteName: '✹ Yeta Studio™',
+		images: [
+			{
+				url: new URL(
+					'https://res.cloudinary.com/doowhwdzj/image/upload/v1749262473/thumbnail_aafvs3.png'
+				),
+			},
+		],
+	},
+	twitter: {
+		title: '✹ Yeta Studio™',
+		description: "'Cause 𝗴͟𝗿͟𝗲͟𝗮͟𝘁 brands aren't built on 𝗹𝘂̶𝗰̶𝗸̶.",
+		images: [
+			new URL(
+				'https://res.cloudinary.com/doowhwdzj/image/upload/v1749262473/thumbnail_aafvs3.png'
+			),
+		],
+	},
 };
 
 //! Fonts
 const editorialNewLight = localFont({
-  src: '../../public/fonts/PPEditorialNew-Ultralight.otf',
-  variable: '--font-editorial-new-light',
-  weight: '400',
+	src: '../../public/fonts/PPEditorialNew-Ultralight.otf',
+	variable: '--font-editorial-new-light',
+	weight: '400',
 });
 
 const editorialNewRegular = localFont({
-  src: '../../public/fonts/PPEditorialNew-Regular.otf',
-  variable: '--font-editorial-new',
-  weight: '400',
+	src: '../../public/fonts/PPEditorialNew-Regular.otf',
+	variable: '--font-editorial-new',
+	weight: '400',
 });
 
 const editorialNewItalic = localFont({
-  src: '../../public/fonts/PPEditorialNew-Italic.otf',
-  variable: '--font-editorial-new-italic',
-  weight: '400',
+	src: '../../public/fonts/PPEditorialNew-Italic.otf',
+	variable: '--font-editorial-new-italic',
+	weight: '400',
 });
 
 const funnelDisplayLight = localFont({
-  src: '../../public/fonts/FunnelDisplay-Light.woff2',
-  variable: "--font-funnel-display-light",
-  weight: "300",
+	src: '../../public/fonts/FunnelDisplay-Light.woff2',
+	variable: '--font-funnel-display-light',
+	weight: '300',
 });
+
 const funnelDisplayRegular = localFont({
-  src: '../../public/fonts/FunnelDisplay-Regular.woff2',
-  variable: "--font-funnel-display-regular",
-  weight: "400",
+	src: '../../public/fonts/FunnelDisplay-Regular.woff2',
+	variable: '--font-funnel-display-regular',
+	weight: '400',
 });
+
 const funnelDisplayMedium = localFont({
-  src: '../../public/fonts/FunnelDisplay-Medium.woff2',
-  variable: "--font-funnel-display-medium",
-  weight: "500",
+	src: '../../public/fonts/FunnelDisplay-Medium.woff2',
+	variable: '--font-funnel-display-medium',
+	weight: '500',
 });
+
 const funnelDisplayBold = localFont({
-  src: '../../public/fonts/FunnelDisplay-Bold.woff2',
-  variable: "--font-funnel-display-bold",
-  weight: "700",
+	src: '../../public/fonts/FunnelDisplay-Bold.woff2',
+	variable: '--font-funnel-display-bold',
+	weight: '700',
 });
 
 //! UI Core
 import Cursor from '@/components/ui/cursor';
 import ReactLenis from 'lenis/react';
 
-
 //! Styles
-import "./globals.css";
+import './globals.css';
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body className={`${funnelDisplayLight.variable} ${funnelDisplayRegular.variable} 
+	return (
+		<html lang="en">
+			<body
+				className={`${funnelDisplayLight.variable} ${funnelDisplayRegular.variable} 
       ${funnelDisplayMedium.variable} ${funnelDisplayBold.variable}
       ${editorialNewLight.variable} ${editorialNewRegular.variable} ${editorialNewItalic.variable}
-      `}>
-        <ReactLenis options={{ lerp: 0.1, duration: 1.7, smoothWheel: true }} root>
-          <ThemeProvider>
-            <Cursor />
-            {children}
-          </ThemeProvider>
-        </ReactLenis>
-      </body>
-    </html>
-  );
+      `}
+			>
+				<ReactLenis
+					options={{ lerp: 0.1, duration: 1.7, smoothWheel: true }}
+					root
+				>
+					<ThemeProvider>
+						<Cursor />
+						{children}
+					</ThemeProvider>
+				</ReactLenis>
+			</body>
+		</html>
+	);
 }
