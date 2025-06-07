e37d499f89a35fbc0cc6022253deff99c4f7d980
Tobías Facello <tobiasfacellodeveloper@gmail.com>
2025-06-07 01:27:14 -0300

    Updates (Components') Cursor component's UI (modifies component color values depending on user theme)

--- src/components/ui/cursor/index.tsx
+++ src/components/ui/cursor/index.tsx
@@ -47,8 +47,8 @@ export default function Cursor() {
     const cursor = cursorRef.current;
 
     mouseRef.current = {
-      x: gsap.quickTo(cursor, "x", { duration: 0.2, ease: Expo.easeOut }),
-      y: gsap.quickTo(cursor, "y", { duration: 0.2, ease: Expo.easeOut })
+      x: gsap.quickTo(cursor, "x", { duration: 0.1, ease: Expo.easeOut }),
+      y: gsap.quickTo(cursor, "y", { duration: 0.1, ease: Expo.easeOut })
     };
 
     const onMouseMove = (e: MouseEvent) => {
@@ -91,11 +91,35 @@ export default function Cursor() {
       xmlns="http://www.w3.org/2000/svg"
     >
       <path
-        fill={isDarkMode ? "var(--background)" : "var(--foreground)"}
-        stroke={isDarkMode ? "var(--foreground)" : "var(--background)"}
+        fill="url(#a)"
+        stroke="url(#b)"
         strokeWidth={1.5}
         d="M1.464 2.825c.995 5.4 2.578 10.843 3.997 15.788a1.003 1.003 0 0 0 1.752.343l4.102-5.22a1 1 0 0 1 .47-.33l5.302-1.768a.996.996 0 0 0 .245-1.77C12.854 6.843 8.16 3.822 3.538 1.322a1.41 1.41 0 0 0-2.074 1.502Z"
       />
+      <defs>
+        <linearGradient
+          id="a"
+          x1={10}
+          x2={10}
+          y1={0}
+          y2={20.5}
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stopColor={isDarkMode ? "#111307" : "#C5C7BC"} />
+          <stop offset={1} stopColor={isDarkMode ? "#0A0B04" : "#60615C"} />
+        </linearGradient>
+        <linearGradient
+          id="b"
+          x1={10}
+          x2={10}
+          y1={0}
+          y2={20.5}
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stopColor={isDarkMode ? "#C5C7BC" : "#111307"} />
+          <stop offset={1} stopColor={isDarkMode ? "#60615C" : "#0A0B04"} />
+        </linearGradient>
+      </defs>
     </svg >
   );
 };
